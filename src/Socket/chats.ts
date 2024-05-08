56b4f93e72898913b319f02c13fca206fb087134
Krz <jrrobertokrz@gmail.com>
2024-05-08 13:51:19 -0300

    fix: flush group messages

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -955,6 +955,7 @@ export const makeChatsSocket = (config: SocketConfig) => {
 			if(!authState.creds?.myAppStateKeyId && !config.mobile) {
 				ev.buffer()
 				needToFlushWithAppStateSync = true
+				ev.flush()
 			}
 		}
 	})
